fd67d9c8cf2e2235f8e5af19d5a47cd39e1b3379
hetit15 <hetbpatel.05@gmail.com>
2024-06-04 11:06:09 +0530

    Add files via upload

---
+++ SE_TOPS/ASSIGNMENT/logic programs_3/array_function/string program/2.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char str[100]; 
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str); 
+
+    printf("Individual characters: ");
+    
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        printf("%c ", str[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
