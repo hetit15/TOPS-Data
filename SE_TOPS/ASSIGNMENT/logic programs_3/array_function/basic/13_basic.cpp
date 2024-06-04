8c098936408205ffea464cc2348079180dc9107b
hetit15 <hetbpatel.05@gmail.com>
2024-06-04 11:10:45 +0530

    Delete SE_TOPS/ASSIGNMENT/logic programs_3/array_function directory

--- SE_TOPS/ASSIGNMENT/logic programs_3/array_function/basic/13_basic.cpp
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-main(){
-	int a = 65;
-char c = a;
-printf("%c", c);
-}
--- SE_TOPS/ASSIGNMENT/logic programs_3/array_function/basic/circumference_rec.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-main(){
-	int a;
-	printf("enter a \n ");
-	scanf("%d",&a);
-	printf("circumference of rectangle is %d ",4*a);
-	
-	
-}
